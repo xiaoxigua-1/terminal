1f7e70dae16fd8f81c11bfef0640a232285071ff
Young <young20050727@gmail.com>
2021-11-15 13:17:16 +0800

    先這樣吧

--- src/util/commands/echo.ts
+++ src/util/commands/echo.ts
@@ -2,17 +2,20 @@ import Command from '../Command';
 import { CommandReturnEcho } from '../CommandReturnEcho';
 
 export default class EchoCommand extends Command {
+  private value: string[] = [];
+
   constructor() {
     super(':Echo [input string]', 'echo');
   }
 
   setValue(args: string[]) {
     console.log(args, this.name);
+    this.value = ['input string'];
   }
 
   run(args: string[], inputPath: string): CommandReturnEcho {
     return {
-      output: `${this.name}`,
+      output: `${this.value}`,
       path: inputPath,
     };
   }
