323e3c19c4aa474ff9d7dd464ee8aefc6a6d7efc
xiao xigua <aijdfajodwsdf@gmail.com>
2021-11-24 00:27:45 +0800

    idk

--- src/util/commandNodes/And.ts
+++ src/util/commandNodes/And.ts
@@ -1,49 +1,13 @@
-import Command from '../Command';
 import Node from './Node';
-import { CommandReturnInfo } from '../data/CommandReturnInfo';
+import CommandManager from '../CommandManage';
 
 export default class AndNode extends Node {
   constructor(right: Node, left: Node, args: string[]) {
     super(left, right, args);
   }
 
-  async* run(path: string, commands: Command[]) {
-    const commandName = this.args[0];
-    const seatchNode = commands.find((command) => command.name === commandName);
-    let left: CommandReturnInfo = {
-      path,
-      output: '',
-      error: false,
-    };
-
-    if (seatchNode) {
-      const info = seatchNode.run(this.args, path);
-      let commandReturnInfo = await info.next();
-      while (!commandReturnInfo.done) {
-        yield {
-          output: commandReturnInfo.value.output,
-          path: commandReturnInfo.value.path,
-          error: commandReturnInfo.value.error,
-        };
-
-        left = {
-          path: commandReturnInfo.value.path,
-          output: '',
-          error: true,
-        };
-
-        // eslint-disable-next-line no-await-in-loop
-        commandReturnInfo = await info.next();
-      }
-    } else {
-      left = {
-        output: `\nCommand  '${commandName}'  is  not  found\n\n`,
-        error: true,
-        path,
-      };
-    }
-
-    const right = this.right.run(left.path, commands);
+  async* run(path: string, commandManager: CommandManager) {
+    const right = this.right.run(this.left.path, commandManager);
     let commandReturnInfo = await right.next();
 
     while (!commandReturnInfo.done) {
--- src/util/commandNodes/Node.ts
+++ src/util/commandNodes/Node.ts
@@ -1,8 +1,15 @@
-import Command from '../Command';
+import CommandManager from '../CommandManage';
 import { CommandReturnInfo } from '../data/CommandReturnInfo';
 
+export interface CommandLeftInfo {
+  output: string[];
+  path: string;
+  error: boolean;
+}
 export default abstract class Node {
   private _right: Node;
+
+  private _left!: CommandLeftInfo;
 
   private _args: string[];
 
@@ -11,7 +18,7 @@ export default abstract class Node {
     this._args = args;
   }
 
-  async* run(path: string, _commands: Command[]):
+  async* run(path: string, _commandManager: CommandManager):
     AsyncGenerator<CommandReturnInfo, void, CommandReturnInfo> {
     yield {
       path,
@@ -20,8 +27,48 @@ export default abstract class Node {
     };
   }
 
+  async* init(path: string, commandManager: CommandManager) {
+    const commandName = this.args[0];
+    const seatchNode = commandManager.commands.find((command) => command.name === commandName);
+    this._left = {
+      path,
+      output: [],
+      error: false,
+    };
+
+    if (seatchNode) {
+      const info = seatchNode.init(this.args, path, commandManager);
+      let commandReturnInfo = await info.next();
+
+      while (!commandReturnInfo.done) {
+        yield {
+          output: commandReturnInfo.value.output,
+          path: commandReturnInfo.value.path,
+          error: commandReturnInfo.value.error,
+        };
+
+        this._left.path = commandReturnInfo.value.path;
+        this._left.output.push(commandReturnInfo.value.output.toString());
+        // eslint-disable-next-line no-await-in-loop
+        commandReturnInfo = await info.next();
+      }
+    } else {
+      yield {
+        output: `\nCommand  '${commandName}'  is  not  found\n\n`,
+        error: true,
+        path,
+      };
+
+      this._left.error = true;
+    }
+  }
+
   public get right(): Node {
     return this._right;
+  }
+
+  public get left(): CommandLeftInfo {
+    return this._left;
   }
 
   public get args(): string[] {
