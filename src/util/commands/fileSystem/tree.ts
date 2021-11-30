3ff0f1ca2c0be574af521131332fb912bbe7d6e8
xiao xigua <aijdfajodwsdf@gmail.com>
2021-11-30 20:45:44 +0800

    add friend links

--- src/util/commands/fileSystem/tree.ts
+++ src/util/commands/fileSystem/tree.ts
@@ -7,6 +7,7 @@ const fileTree = new Folder('', [
     new Folder('xiaoxigua', [
       new TextFile('about.txt', '喔喔不行\n我快不行了\n\n\n\n\n\nYoung把我電的不要不要的', 'xiaoxigua'),
       new Folder('project', [], 'xiaoxigua'),
+      new TextFile('friends', 'my website: https://xiaoxigua-1.github.io/\n\n\nhttps://ntihs-it.github.io/#/', 'xiaoxigua'),
     ], 'xiaoxigua'),
   ], 'xiaoxigua'),
 ], 'xiaoxigua');
